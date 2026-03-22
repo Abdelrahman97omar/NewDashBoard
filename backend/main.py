fa9456b3c8bcf7bcc5c5da4966a5e4c0a2881e99
abdelrahman <abdelrahman.omar@marses.systems>
2026-03-22 04:30:31 +0200

    Remove preserving the last pressed button in the side bar from redis and save it in session local storage

--- backend/main.py
+++ backend/main.py
@@ -31,19 +31,3 @@ async def returnCurrentStates():
 @app.get("/{button_number}")
 async def returnMainState(button_number: int):
     return {"The Button_number": button_number}
-
-@app.get("/button/sidebarButtonState")
-async def getSidebarButtonState():
-    lastbutton=r.get("lastButton")
-    if lastbutton:
-        lastbutton = lastbutton.decode()
-    return({"lastbutton":lastbutton})
-
-@app.put("/button/setsidebarLastButton")
-async def setsidebarLastButton(data: ButtonData):
-    print("I recieved a request")
-    try:
-        r.set("lastButton", data.button_value)
-        print(f"The lastbutton {data.button_value} is set in redis")
-    except Exception as e:
-        print(f"Error setting value in redis: {e}")
--- src/Components/Sidebar/Sidebar.tsx
+++ src/Components/Sidebar/Sidebar.tsx
@@ -7,106 +7,55 @@ type myreturnNumber={
 
 const Sidebar =({returnNumber}:myreturnNumber)=>{
 
-const [isPresesd,setIsPressed]=useState(0)
+let lastPressedButton=sessionStorage.getItem("currentPage")
+if (lastPressedButton===null){lastPressedButton="Control"}
+const [isPresesd,setIsPressed]=useState(lastPressedButton)
 
-const SetButtonNumber=(sendbutton:String)=>{
-  const sendButtonStateRedis=async ()=>{
-    await fetch("http://127.0.0.1:8001/button/setsidebarLastButton",{method:"PUT",
-      headers:{"Content-Type": "application/json"},body: JSON.stringify({button_value: sendbutton})})
-      }
-    sendButtonStateRedis()
+
+const SetButtonNumber=(sendbutton:string)=>{ 
+  sessionStorage.setItem("currentPage",sendbutton)
       switch (sendbutton) {
         case "Control":
-          setIsPressed(0);
+          setIsPressed("Control");
           returnNumber("Control")
           break;
         case "Settings":
-          setIsPressed(1);
+          setIsPressed("Settings");
           returnNumber("Settings")
           break;
         case "Event mode":
-          setIsPressed(2);
+          setIsPressed("Event mode");
           returnNumber("Event mode")
           break;
         case "Table Mode":
-          setIsPressed(3);
+          setIsPressed("Table Mode");
           returnNumber("Table Mode")
           break;
         case "Specialization":
-          setIsPressed(4);
+          setIsPressed("Specialization");
           returnNumber("Specialization")
           break;
         case "Information":
-          setIsPressed(5);
+          setIsPressed("Information");
           returnNumber("Information")
           break;
         case "Wifi Settings":
-          setIsPressed(6);
+          setIsPressed("Wifi Settings");
           returnNumber("Wifi Settings")
           break;
       }
 }
 
-
-useEffect(() => {
-  const fetchData = async () => {
-    const resp = await fetch("http://127.0.0.1:8001/button/sidebarButtonState",{method:"GET"})
-    const data = await resp.json()
-
-    const sendbutton=data["lastbutton"]
-    
-    switch (sendbutton) {
-      case "Control":
-        setIsPressed(0);
-        returnNumber("Control")
-        break;
-      case "Settings":
-        setIsPressed(1);
-        returnNumber("Settings")
-        break;
-      case "Event mode":
-        setIsPressed(2);
-        returnNumber("Event mode")
-        break;
-      case "Table Mode":
-        setIsPressed(3);
-        returnNumber("Table Mode")
-        break;
-      case "Specialization":
-        setIsPressed(4);
-        returnNumber("Specialization")
-        break;
-      case "Information":
-        setIsPressed(5);
-        returnNumber("Information")
-        break;
-      case "Wifi Settings":
-        setIsPressed(6);
-        returnNumber("Wifi Settings")
-        break;
-    }
-
-  }
-  fetchData()
-})
-
   return (
     <div className="bg-stone-600 rounded-l-lg p-3">
       <div className="grid grid-cols-1 px-8 py-10">
-        <button className={isPresesd===0? "pressedSidebarButtons":"sidebarButtons"} onClick={()=>SetButtonNumber("Control")}>Control</button>
-        <button className={isPresesd===1? "pressedSidebarButtons":"sidebarButtons"} onClick={()=>SetButtonNumber("Settings")}>Settings</button>
-        <button className={isPresesd===2? "pressedSidebarButtons":"sidebarButtons"} onClick={()=>SetButtonNumber("Event mode")}>Event mode</button>
-        <button className={isPresesd===3? "pressedSidebarButtons":"sidebarButtons"} onClick={()=>SetButtonNumber("Table Mode")}>Table Mode</button>
-        <button className={isPresesd===4? "pressedSidebarButtons":"sidebarButtons"} onClick={()=>SetButtonNumber("Specialization")}>Specialization</button>
-        <button className={isPresesd===5? "pressedSidebarButtons":"sidebarButtons"} onClick={()=>SetButtonNumber("Information")}>Information</button>
-        <button className={isPresesd===6? "pressedSidebarButtons":"sidebarButtons"} onClick={()=>SetButtonNumber("Wifi Settings")}>Wifi Settings</button>
-        {/* <button className="sidebarButtons " onClick={()=>returnNumber("Control")}>Control</button>
-        <button className="sidebarButtons " onClick={()=>returnNumber("Settings")}>Settings</button>
-        <button className="sidebarButtons " onClick={()=>returnNumber("Event mode")}>Event mode</button>
-        <button className="sidebarButtons " onClick={()=>returnNumber("Table Mode")}>Table Mode</button>
-        <button className="sidebarButtons " onClick={()=>returnNumber("Specialization")}>Specialization</button>
-        <button className="sidebarButtons " onClick={()=>returnNumber("Information")}>Information</button>
-        <button className="sidebarButtons " onClick={()=>returnNumber("Wifi Settings")}>Wifi Settings</button> */}
+        <button className={isPresesd==="Control"? "pressedSidebarButtons":"sidebarButtons"} onClick={()=>SetButtonNumber("Control")}>Control</button>
+        <button className={isPresesd==="Settings"? "pressedSidebarButtons":"sidebarButtons"} onClick={()=>SetButtonNumber("Settings")}>Settings</button>
+        <button className={isPresesd==="Event mode"? "pressedSidebarButtons":"sidebarButtons"} onClick={()=>SetButtonNumber("Event mode")}>Event mode</button>
+        <button className={isPresesd==="Table Mode"? "pressedSidebarButtons":"sidebarButtons"} onClick={()=>SetButtonNumber("Table Mode")}>Table Mode</button>
+        <button className={isPresesd==="Specialization"? "pressedSidebarButtons":"sidebarButtons"} onClick={()=>SetButtonNumber("Specialization")}>Specialization</button>
+        <button className={isPresesd==="Information"? "pressedSidebarButtons":"sidebarButtons"} onClick={()=>SetButtonNumber("Information")}>Information</button>
+        <button className={isPresesd==="Wifi Settings"? "pressedSidebarButtons":"sidebarButtons"} onClick={()=>SetButtonNumber("Wifi Settings")}>Wifi Settings</button>
       </div>
     </div>
   )
