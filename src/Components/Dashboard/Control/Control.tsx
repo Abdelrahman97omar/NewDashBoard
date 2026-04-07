114483bead72695dbd61191bcf9f918541ffe956
abdelrahman <abdelrahman.omar@marses.systems>
2026-04-07 15:23:42 +0200

    fix bug in manual button

--- src/Components/Dashboard/Control/Control.tsx
+++ src/Components/Dashboard/Control/Control.tsx
@@ -22,9 +22,11 @@ const Control=()=>{
         const res = await fetch (`http://${window.location.hostname}:8001/stausBar/States`, {method:"GET"})
         const data = await res.json()
         const robotMode= Number(JSON.parse(data["manual_auto_mode"]))
-        setisManual(robotMode ?? 0)
         console.log("FULL RESPONSE:", data)
         console.log("manual_auto_mode:", data["manual_auto_mode"])
+        console.log("robotMode:", robotMode)
+        setisManual(robotMode ?? 0)
+
     }
 
     useEffect(()=>{
