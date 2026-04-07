204d67ee24af24fc6e18a2a50f4a25c181855479
abdelrahman <abdelrahman.omar@marses.systems>
2026-04-07 13:42:32 +0200

    Stop tracking launch files

--- launchApp.sh
+++
@@ -1,5 +0,0 @@
-echo 246810 | sudo -S ufw allow 8001
-sudo ufw allow 9090  
-sudo ufw allow 9876  
-sudo ufw allow 5173  
-docker run -it --net='host' -v '/home/abdelrahman/Event Apps/NewDashBoard':/home/backend newdashboard:latest; exec bash
