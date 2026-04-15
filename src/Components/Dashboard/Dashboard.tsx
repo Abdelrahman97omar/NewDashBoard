6dbb14f997eaa680a8218256a500eff795f13ec1
abdelrahman <abdelrahman.omar@marses.systems>
2026-04-15 15:38:09 +0200

    remove faild test in dashboard.tsx and eventmode.tsx

--- src/Components/Dashboard/Dashboard.tsx
+++ src/Components/Dashboard/Dashboard.tsx
@@ -4,55 +4,14 @@ import Table from "./Table/Table";
 import SettingS from "./Settings/Settings";
 import EventMode from "./EventMode/EventMode";
 import Control from "./Control/Control";
-import WifiSettings from "./wifiSettings/wifiSettings";
-import { useEffect, useState } from "react";
-import { useRosConnection } from "../../connection-provider";
+import WifiSettings from "./wifiSettings/wifiSettings"
 
 type MyProp = {
   page: number;
 };
 
 const Dashboard = ({ page }: MyProp) => {
-  const [alive_X, setLive_X] = useState("0");
-  const [alive_Y, setLive_Y] = useState("0");
-  const [alive_Seta, setLive_SETA] = useState("0");
-  const { subscribeTopic} = useRosConnection();
 
-
-
-
-    useEffect(() => {
-      console.log(alive_X, alive_Y, alive_Seta);
-    }, [alive_X, alive_Y, alive_Seta]);
-
-  useEffect(() => {
-    const livePointsUpdate = (message: any) => {
-      const yaw = Math.atan2(
-        2 *
-          (message.pose.pose.orientation.w * message.pose.pose.orientation.z +
-            message.pose.pose.orientation.x * message.pose.pose.orientation.y),
-        1 -
-          2 *
-            (message.pose.pose.orientation.y * message.pose.pose.orientation.y +
-              message.pose.pose.orientation.z * message.pose.pose.orientation.z)
-      );
-      const yawDeg: any = yaw * (180 / Math.PI);
-      const X = Number(message.pose.pose.position.x).toFixed(2);
-      const Y = Number(message.pose.pose.position.y).toFixed(2);
-      const Seta = Number(yawDeg).toFixed(2);
-      setLive_X(X);
-      setLive_Y(Y);
-      setLive_SETA(Seta);
-      console.log(alive_X);
-      console.log(alive_Y);
-      console.log(alive_Seta);
-    };
-    subscribeTopic(
-      "/slamware_ros_sdk_server_node/odom",
-      "nav_msgs/Odometry",
-      livePointsUpdate
-    );
-  }, []);
   //Control Button
   if (page === 0) {
     return <Control />;
@@ -63,9 +22,7 @@ const Dashboard = ({ page }: MyProp) => {
   }
   //Event Mode Button
   if (page === 2) {
-    return (
-      <EventMode live_X={alive_X} live_Y={alive_Y} live_Seta={alive_Seta} />
-    );
+    return <EventMode />;
   }
   //Table settings Mode
   if (page === 3) {
